9e5f0aae986e7296501b234e81e5ae36f19eb36a
Michael Nascimento <michael_thewebmaster@iMac-de-Michael.local>
2020-08-24 21:44:08 -0300

    ensure LoadUserByEmailRepository throws if no email is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,4 +1,5 @@
 const MongoHelper = require('../helpers/mongo-helper')
+const MissingParamError = require('../../utils/errors/missing-param-error')
 let db
 
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
@@ -53,5 +54,11 @@ describe('LoadUserByEmail Repository', () => {
     const sut = new LoadUserByEmailRepository()
     const promise = sut.load('any_email@mail.com')
     expect(promise).rejects.toThrow()
+  })
+
+  test('Should throw if no email is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.load()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })

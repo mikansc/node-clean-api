7fd1f45bb842d3d290d366013c5a2e4ad7de14ed
Michael Nascimento <michael_thewebmaster@iMac-de-Michael.local>
2020-08-26 14:12:23 -0300

    add routes file

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const setupApp = require('./setup')
+const setupRoots = require('./routes')
 
 setupApp(app)
+setupRoots(app)
 
 module.exports = app
